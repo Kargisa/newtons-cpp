c4aee0f4fe472b8714bbea07fda3013ab61a5950
Kargisa <luca.wolf@outlook.com>
2024-09-01 17:48:55 +0200

    Init

---
+++ newtons-cpp/newtons-cpp.cpp
@@ -0,0 +1,13 @@
+﻿// newtons-cpp.cpp : Defines the entry point for the application.
+//
+
+#include "newtons-cpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << "Hello Bobsch." << endl;
+	return 0;
+}
